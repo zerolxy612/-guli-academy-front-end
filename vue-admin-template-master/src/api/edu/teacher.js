a5ee27511dc818620430a663227eb3d3842f6d78
zerolxy <1056180778@qq.com>
2022-11-16 17:44:33 +0800

    '讲师添加及删除功能完成'

--- vue-admin-template-master/src/api/edu/teacher.js
+++ vue-admin-template-master/src/api/edu/teacher.js
@@ -3,9 +3,24 @@ import request from '@/utils/request';
 export default {
   getTeacherListPage(current,limit,teacherQuery) {
     return request({
-      url: `/eduservice/teacher/pageTeacher/${current}/${limit}`,
-      method: 'get',
+      url: `/eduservice/teacher/pageTeacherCondition/${current}/${limit}`,
+      method: 'post',
       data: teacherQuery
+    });
+  },
+  // 删除讲师的方法
+  deleteTeacherId(id) {
+    return request({
+      url: `/eduservice/teacher/${id}`,
+      method: 'delete',
+    });
+  },
+  // 添加讲师的方法
+  addTeacher(teacher){
+    return request({
+      url: `/eduservice/teacher/addTeacher`,
+      method: 'post',
+      data: teacher
     });
   }
 };
